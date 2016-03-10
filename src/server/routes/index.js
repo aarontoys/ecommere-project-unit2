13f49ce396b0d3610d4824e22eba7405eaefec20
Aaron Toys <aarontoys@gmail.com>
2016-03-10 15:48:27 -0700

    add test stripe module

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../../../db/knex');
+var stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -21,14 +25,19 @@ router.get('/invoice', function(req, res, next) {
   res.render('invoice', {});
 });
 
+router.get('/charge', function (req, res, next) {
+  res.render('charge');
+})
+
 router.post('/charge', function(req, res,next) {
+    console.log(req.body);
     var stripeToken = req.body.stripeToken;
     var amount =  req.body.stripeAmount;
 
     stripe.charges.create({
         card: stripeToken,
         currency: 'usd',
-        amount: amount
+        amount: 10000
     },
     function(err, charge) {
         if (err) {
